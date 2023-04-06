b51d555fd11ecbb832bf8ba8519b1eed585a151b
Ferhat Iltas <iltasferhat@gmail.com>
2023-04-06 08:54:16 +0300

    Added selected multi item.

--- lib/features/puzzle/view/puzzle_view.dart
+++ lib/features/puzzle/view/puzzle_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sizer/sizer.dart';
 
 class PuzzleView extends StatefulWidget {
   const PuzzleView({super.key});
@@ -9,39 +8,155 @@ class PuzzleView extends StatefulWidget {
 }
 
 class _PuzzleViewState extends State<PuzzleView> {
+  GlobalKey gridKey = new GlobalKey();
+
+  List<List<String>> gridState = [
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+  ];
+  var indexColor = Colors.white;
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: Colors.red,
-        width: 100.w,
-        height: 100.h,
-        child: SizedBox(
-          width: 100.w,
-          height: 60.h,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: GridView.builder(
-                itemCount: 81,
-                physics: const NeverScrollableScrollPhysics(),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 9,
-                  crossAxisSpacing: 5.0,
-                  mainAxisSpacing: 5.0,
-                ),
-                itemBuilder: (context, index) {
-                  return Container(
-                    color: index % 2 == 0 ? Colors.white : Colors.grey,
-                    child: Center(
-                        child: Text(
-                      index.toString(),
-                      style: TextStyle(fontSize: 16.sp),
-                    )),
-                  );
-                }),
+  void initState() {
+    super.initState();
+  }
+
+  Widget _buildBody() {
+    int gridStateLength = gridState.length;
+    return Column(children: <Widget>[
+      AspectRatio(
+        aspectRatio: 1.0,
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: GridView.builder(
+            key: gridKey,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: gridStateLength,
+              //childAspectRatio: 8.0 / 11.9
+            ),
+            itemBuilder: _buildGridItems,
+            itemCount: gridStateLength * gridStateLength,
+          ),
+        ),
+      ),
+    ]);
+  }
+
+  Widget _buildGridItems(BuildContext context, int index) {
+    int gridStateLength = gridState.length;
+    int x, y = 0;
+    x = (index / gridStateLength).floor();
+    y = (index % gridStateLength);
+    GlobalKey gridItemKey = new GlobalKey();
+
+    return GestureDetector(
+      onTapDown: (details) {
+        for (var i = 0; i < gridState.length; i++) {
+          for (var j = 0; j < gridState[i].length; j++) {
+            gridState[i][j] = "";
+          }
+        }
+
+        var _box = gridItemKey.currentContext?.findRenderObject();
+        var _boxGrid = gridKey.currentContext?.findRenderObject();
+        var position = (_boxGrid as RenderBox).localToGlobal(Offset.zero); //this is global position
+        double gridLeft = position.dx;
+        double gridTop = position.dy;
+
+        double gridPosition = details.globalPosition.dy - gridTop;
+
+        //Get item position
+        int indexX = (gridPosition / (_box as RenderBox).size.width).floor().toInt();
+        int indexY = ((details.globalPosition.dx - gridLeft) / _box.size.width).floor().toInt();
+
+        if (gridState[indexX][indexY] == "Y") {
+          gridState[indexX][indexY] = "";
+        } else {
+          gridState[indexX][indexY] = "Y";
+          indexColor = Colors.green;
+        }
+
+        setState(() {});
+      },
+      onVerticalDragUpdate: (details) {
+        selectItem(gridItemKey, details);
+      },
+      onHorizontalDragUpdate: (details) {
+        selectItem(gridItemKey, details);
+      },
+      child: GridTile(
+        key: gridItemKey,
+        child: Container(
+          decoration: BoxDecoration(border: Border.all(color: Colors.black, width: 0.5)),
+          child: Center(
+            child: _buildGridItem(x, y),
           ),
         ),
       ),
     );
+  }
+
+  void selectItem(GlobalKey<State<StatefulWidget>> gridItemKey, var details) {
+    var _boxItem = gridItemKey.currentContext?.findRenderObject();
+    var _boxMainGrid = gridKey.currentContext?.findRenderObject();
+    Offset position = (_boxMainGrid as RenderBox).localToGlobal(Offset.zero); //this is global position
+    double gridLeft = position.dx;
+    double gridTop = position.dy;
+
+    double gridPosition = details.globalPosition.dy - gridTop;
+
+    //Get item position
+    int rowIndex = (gridPosition / (_boxItem as RenderBox).size.width).floor().toInt();
+    int colIndex = ((details.globalPosition.dx - gridLeft) / _boxItem.size.width).floor().toInt();
+    gridState[rowIndex][colIndex] = "Y";
+    indexColor = Colors.green;
+
+    setState(() {});
+  }
+
+  Widget _buildGridItem(int x, int y) {
+    switch (gridState[x][y]) {
+      case '':
+        return Text('s');
+      case 'Y':
+        return Container(
+          color: indexColor,
+        );
+      case 'N':
+        return Container(
+          color: Colors.white,
+        );
+      default:
+        return Text(gridState[x][y].toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('widget.title'),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _buildBody(),
+          ],
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  _gridItemTapped(int x, int y) {
+    print("x is $x and Y is $y");
   }
 }
